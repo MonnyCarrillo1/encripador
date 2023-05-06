11dded11e9c9a6f1be454952b0f49d14743dfee2
Myou_ <Myou_festa@outlook.com>
2023-05-06 13:33:02 -0600

    proyecto terminado

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".text-area2");
+const imagen = document.querySelector(".muñeco");
+const p = document.querySelector(".mensaje1");
 
-
-const matrizCodigo = {
-  e: "enter",
-  i: "imes",
-  o: "ober",
-  a: "ai",
-  u: "ufat"
-};
-
-
-function encryptButton() {
-  const textoEncriptado = encrypt(textArea.value)
-  console.log(textoEncriptado);
-  return textoEncriptado;
+// FUNCION PARA ENCRIPTAR
+const encryptText = (text) => {
+  let encryptedText = text.replace(/e/g, "enter");
+  encryptedText = encryptedText.replace(/i/g, "imes");
+  encryptedText = encryptedText.replace(/a/g, "ai");
+  encryptedText = encryptedText.replace(/o/g, "ober");
+  encryptedText = encryptedText.replace(/u/g, "ufat");
+  return encryptedText;
 }
 
-
-function encrypt(stringEncriptada) {
-
-  for (const key in matrizCodigo) {
-    const regex = new RegExp(key, "g");
-    stringEncriptada = stringEncriptada.replace(regex, matrizCodigo[key]);
-  }
-  return stringEncriptada
+// FUNCION PARA DESENCRIPTAR
+const decryptText = (text) => {
+  let decryptedText = text.replace(/enter/g, "e");
+  decryptedText = decryptedText.replace(/imes/g, "i");
+  decryptedText = decryptedText.replace(/ai/g, "a");
+  decryptedText = decryptedText.replace(/ober/g, "o");
+  decryptedText = decryptedText.replace(/ufat/g, "u");
+  return decryptedText;
 }
 
-console.log(encryptButton(textArea));
+document.querySelector("#encryptButton").addEventListener("click", () => {
+  const inputText = textArea.value.toLowerCase();
+  const encryptedText = encryptText(inputText);
 
+  
+  p.innerHTML = encryptedText
+  imagen.style.display = 'none';
+  mensaje.innerHTML = "";
+  
+  // mostrar el resultado
+  resultArea.style.display = "block";
+});
 
+// AÑADIR FUNCION AL BOTON DESCENCRIPTAR
+document.querySelector("#decryptButton").addEventListener("click", () => {
+  const inputText = textArea.value.toLowerCase();
+  const decryptedText = decryptText(inputText);
 
+ 
+  p.innerHTML = decryptedText
+  imagen.style.display = 'none';
+  mensaje.innerHTML = "";
+  
 
+  // mostrar el resultado
+  resultArea.style.display = "block";
+});
+
+// DEFINIR EL BOTON PARA COPIAR
+const copyBtn = document.getElementById('copyButton');
+
+// AÑADIR FUNCION AL BOTON 
+copyBtn.addEventListener('click', () => {
+    const resultArea = document.getElementById('result');
+    const resultText = resultArea.innerText;
+    navigator.clipboard.writeText(resultText);
+    alert("Texto copiado al portapapeles");
+});
